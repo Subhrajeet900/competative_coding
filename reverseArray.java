91c1d2e572620ec8800c18a33383b6bfb8c23727
subhrajeet dash <subhrajeetdash900@gmail.com>
2025-09-27 11:03:04 +0530

    Update reverseArray.java

--- reverseArray.java
+++ reverseArray.java
@@ -1,3 +1,9 @@
+// 1. Using Two-Pointer / Swapping (In-place)
+// Start from the first (left = 0) and last index (right = n-1).
+// Swap elements at left and right.
+// Move left++ and right-- until they cross.
+// Efficient: O(n) time, O(1) extra space.
+    
 class Solution {
     public void reverseArray(int arr[]) {
         // code here
@@ -15,4 +21,4 @@ class Solution {
     }
 }
 
-// time complexity = O(n)  and space complexity = O(1)
+
